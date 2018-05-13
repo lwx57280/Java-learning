ec0791e9e65e72fc9d4454a0bb191e6e62b499e1
lwx57280 <448341911@qq.com>
2018-05-13 17:28:24 +0800

    【java 对象与类，类和静态导入】

--- chapter4.7/src/com/packageTest.java
+++ chapter4.7/src/com/packageTest.java
@@ -1,20 +1,30 @@
 package com;
 
 import com.com.horstman.corejava.Employee;
+
 import static java.lang.System.*;
+
 /**
- * 该程序演示了包的使用。
- *类的导入和静态导入
- * @see com.com.horstman.corejava#raiseSalary(double)
+ * @Description: 该程序演示了包的使用。
+ * 类的导入和静态导入
+ * @Author: yc
+ * @CreateDate: 2018/5/7 22:51
+ * @UpdateUser: yc
+ * @UpdateDate: 2018/5/7 22:51
+ * @UpdateRemark: 修改内容
+ * @Version: 1.0
  */
 public class packageTest {
-    public static void main(String[] args){
 
-        Employee harry = new Employee("Harry Hacker",50000,1989,10,1);
+    public static void main(String[] args) {
+
+        Employee harry = new Employee("Harry Hacker", 50000, 1989, 10, 1);
         harry.raiseSalary(5);
-
+        Boolean aTrue=Boolean.TRUE;
+        out.println("aTrue=:"+aTrue);
         // 因为静态导入语句，我们不必使用 System.out
-        out.println("name=:"+harry.getName()+",salary=:"+harry.getSalary());
+        out.println("name=:" + harry.getName() + ",salary=:" + harry.getSalary());
+
     }
 
 }
