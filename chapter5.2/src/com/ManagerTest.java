e5ced7282fa5e9c71f1abe52b20476f988465caf
lwx57280 <448341911@qq.com>
2018-06-15 10:57:09 +0800

    【java 重写Object类的equals方法】

---
+++ chapter5.2/src/com/ManagerTest.java
@@ -0,0 +1,25 @@
+package com;
+/**
+* @Description:     继承层次
+* @Author:         yc
+* @CreateDate:     2018/5/13 19:21
+* @UpdateUser:     yc
+* @UpdateDate:     2018/5/13 19:21
+* @UpdateRemark:   修改内容
+* @Version:        1.0
+*/
+public class ManagerTest {
+    public static void main(String[] args){
+        Manager boss= new Manager("Car Cracker",80000,1987,12,15);
+        boss.setBouns(5000);
+
+        Employee[] staff = new Employee[3];
+        staff[0]=boss;
+        staff[1]=new Employee("Harry Hacker",50000,1989,10,1);
+        staff[2]=new  Employee("Tommy Tester",40000,1990,3,15);
+
+        for (Employee e:staff)
+            System.out.println("name=:"+e.getName()+",salary=:"+e.getSalary()+",HireDay:"+e.getHireDay());
+
+    }
+}
