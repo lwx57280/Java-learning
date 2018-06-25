fd3ecf0d530365d7fbfc933f24b8627b27963b59
lwx57280 <448341911@qq.com>
2018-06-25 09:29:15 +0800

    【java 泛型数组列表】

--- chapter5.3/src/com/hashCodeDemo.java
+++ chapter5.3/src/com/hashCodeDemo.java
@@ -4,6 +4,12 @@ import java.util.ArrayList;
 
 /**
  * 泛型数组列表
+ *
+ * List:
+ * | -- Vector:内部是数组数据结构,是线程安全的。增、删、查都很慢！效率低
+ * | -- ArrayList:内部是数组数据结构，是线程不安全的。替代了Vector。查询的速度很快、增删慢。
+ *  ArrayList是List接口实现类，可以动态调整大小的数组。不同步，默认初始容量为10，当元素超过10个以后
+ * 会重新分配内存空间，并将所有的对象从较小的数组中拷贝到较大的数组中，那么容量变化的规则是(旧容量*3)/2+1
  */
 public class hashCodeDemo {
     public static void main(String[] args) {
