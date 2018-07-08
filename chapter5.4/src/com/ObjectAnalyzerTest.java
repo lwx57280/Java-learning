f9988937c79050e17918d91a933706cf4ba00f82
lwx57280 <448341911@qq.com>
2018-07-08 16:11:20 +0800

    【java 对象包装器与自动装箱拆箱】

--- chapter5.4/src/com/ObjectAnalyzerTest.java
+++ chapter5.4/src/com/ObjectAnalyzerTest.java
@@ -1,5 +1,7 @@
 package com;
 
+import org.omg.CORBA.IntHolder;
+
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
@@ -18,12 +20,40 @@ public class ObjectAnalyzerTest {
         System.out.println(squares.size());
 
         System.out.println(Arrays.toString(squares.toArray()));
-        for (int i=0;i<squares.size();i++) {
+        for (int i = 0; i < squares.size(); i++) {
             // 自动拆箱 int num = ((Integer)squares.get(i)).intValue();
             int num = squares.get(i);
-            System.out.println("num=:"+num);
+            System.out.println("num=:" + num);
             break;
         }
         // System.out.println(new ObjectAnalyzer().toString(squares));
+
+        int a = 8;
+        triple(a);
+//        System.out.printf("%d %s",a,"widgets");
+
+        Integer x = 100;
+        tripleInteger(x);
+
+        IntHolder t = new IntHolder(9);
+        change(t);
+    }
+
+    public static void triple(int x) {
+        // 修改局部变量
+        x = Integer.valueOf(3) * x;
+        System.out.println("x=:" + x);
+    }
+
+    public static void tripleInteger(Integer x) {
+        // 修改局部变量
+        x = 3 * x;
+        System.out.println("x=:" + x);
+    }
+
+    public static void change(IntHolder x) {
+        // 每个持有者类型都包含一个共有域值
+        x.value = 3 * x.value;
+        System.out.println("x=:" + x.value);
     }
 }
