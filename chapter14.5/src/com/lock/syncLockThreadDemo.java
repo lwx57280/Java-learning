552b157a129a9e06406b99017bda3c4d30a36cd8
lwx57280 <448341911@qq.com>
2018-08-26 17:15:00 +0800

    【java ReentrantLock(重入锁)】

--- chapter14.5/src/com/lock/syncLockThreadDemo.java
+++ chapter14.5/src/com/lock/syncLockThreadDemo.java
@@ -1,5 +1,11 @@
 package com.lock;
 
+/**
+ * 效果和前两种方法差不多。
+ * 如果synchronized关键字能满足用户的需求，就用synchronized，因为它能简化代码 。
+ * 如果需要更高级的功能，就用ReentrantLock类，
+ * 此时要注意及时释放锁，否则会出现死锁，通常在finally代码释放锁
+ */
 public class syncLockThreadDemo {
     public static void main(String[] args) {
         final Bank bank = new Bank();
