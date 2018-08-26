91e641fdc8ad0914c0376f3125118304914d74b3
lwx57280 <448341911@qq.com>
2018-08-26 16:38:37 +0800

    【java ReentrantLock(重入锁)】

---
+++ chatper14.4/src/com/thread/demo/Bank.java
@@ -0,0 +1,40 @@
+package com.thread.demo;
+
+import java.util.concurrent.locks.Lock;
+import java.util.concurrent.locks.ReentrantLock;
+
+public class Bank {
+
+    private int count = 0;   // 账号余额
+
+    private Lock lock = new ReentrantLock();
+
+    /**
+     * 存钱
+     */
+    public void addMoney(int money) {
+        // (同步代码块，对于锁的操作是隐式的。 )
+        synchronized (this) {
+            count += money;
+            System.out.println(System.currentTimeMillis() + "存进" + money);
+        }
+    }
+
+    /**
+     * 取钱
+     */
+    public void subMoney(int money) {
+        synchronized (this) {
+            if (count - money < 0) {
+                System.out.println("余额不足！");
+                return;
+            }
+            count -= money;
+            System.out.println(System.currentTimeMillis() + "取钱:" + money);
+        }
+    }
+
+    public void lookMoney(){
+        System.out.println("账户余额:" + count);
+    }
+}
---
+++ chatper14.4/src/com/thread/demo/syncThreadTest.java
@@ -0,0 +1,39 @@
+package com.thread.demo;
+
+/**
+ * 线程同步
+ */
+public class syncThreadTest {
+    public static void main(String[] args) {
+        final Bank bank = new Bank();
+        Thread tadd = new Thread(() -> {
+            while (true) {
+                try {
+                    Thread.sleep(1000);
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+                bank.addMoney(100);
+                bank.lookMoney();
+                System.out.println("\n");
+            }
+        });
+
+        Thread tsub = new Thread(() -> {
+            while (true) {
+                bank.addMoney(100);
+                bank.lookMoney();
+                System.out.println("\n");
+                try {
+                    Thread.sleep(1000);
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+            }
+        });
+
+        tsub.start();
+
+        tadd.start();
+    }
+}
