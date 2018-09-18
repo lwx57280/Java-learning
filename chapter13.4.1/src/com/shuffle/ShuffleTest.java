641c1c66358dc4f825d0d73e871c44b895826a2d
lwx57280 <448341911@qq.com>
2018-09-18 20:13:00 +0800

    【java 随机洗牌和排序算法】

---
+++ chapter13.4.1/src/com/shuffle/ShuffleTest.java
@@ -0,0 +1,23 @@
+package com.shuffle;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
+/**
+ * 随机洗牌和排序算法
+ */
+public class ShuffleTest {
+    public static void main(String[] args) {
+        List<Integer> numbers = new ArrayList<>();
+        for(int i=0;i<=49;i++)
+            numbers.add(i);
+        // 随机打乱列表
+        Collections.shuffle(numbers);
+        System.out.println(numbers);
+        // 从打乱后的列表中选前6个值
+        List<Integer> winningCombination = numbers.subList(0, 6);
+        Collections.sort(winningCombination);
+        System.out.println(winningCombination);
+    }
+}
